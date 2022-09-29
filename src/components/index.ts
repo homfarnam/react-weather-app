c10de16ce1cca8744f347ca92a9649a65e9142bb
homfarnam <homfarnam@gmail.com>
2022-09-29 14:18:46 +0200

    Add index components file

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Header from "./Header/Header"
+import Days from "./Days/Days"
+import Day from "./Day/Day"
+import Title from "./Shared/Title/Title"
+import WeatherData from "./WeatherData/WeatherData"
+
+export { Header, Days, Title, Day, WeatherData }
